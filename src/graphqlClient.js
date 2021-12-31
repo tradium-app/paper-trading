d67e4358082f75826b0c59114bec6087f51b12be
Suraj Shrestha <syuraj@gmail.com>
2021-12-31 02:20:51 -0500

    upgrade packages

--- src/graphqlClient.js
+++ src/graphqlClient.js
@@ -1,8 +1,5 @@
-import dotenv from 'dotenv';
 import { ApolloClient, ApolloLink, createHttpLink, InMemoryCache } from '@apollo/client';
 import DebounceLink from 'apollo-link-debounce';
-
-dotenv.config();
 
 const DEFAULT_DEBOUNCE_TIMEOUT = 500;
 const debounceLink = new DebounceLink(DEFAULT_DEBOUNCE_TIMEOUT);
