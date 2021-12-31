b427f41e711d7c8eecc4359dbe802362d07f4aa5
Suraj Shrestha <syuraj@gmail.com>
2021-12-31 02:26:09 -0500

    remove unused pages & components

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,7 @@
 import { useRoutes } from 'react-router-dom';
-import DashboardRoutes from './DashboardRoutes';
 import MainRoutes from './MainRoutes';
 import config from 'config';
 
 export default function ThemeRoutes() {
-    return useRoutes([MainRoutes, DashboardRoutes], config.basename);
+    return useRoutes([MainRoutes], config.basename);
 }
