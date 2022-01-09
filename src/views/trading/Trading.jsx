0061c0c4be27df470577b9aeef6d15bbc1369bcf
Suraj Shrestha <syuraj@gmail.com>
2022-01-09 04:09:59 -0500

    show market/limit order type even though not functional

--- src/views/trading/Trading.jsx
+++ src/views/trading/Trading.jsx
@@ -1,17 +1,12 @@
-import { useTheme } from '@mui/material/styles'
-import { Grid, Stack, Typography, useMediaQuery } from '@mui/material'
+import { Grid } from '@mui/material'
 
 import BackgroundWrapper from './BackgroundWrapper'
-import FormWrapper from './order/FormWrapper'
 import OrderForm from './order/OrderForm'
 import Chart from './chart/Index'
 import MainCard from 'ui-component/cards/MainCard'
 import OrderHistory from './orderHistory/Index'
 
 const Login = () => {
-	const theme = useTheme()
-	const matchDownSM = useMediaQuery(theme.breakpoints.down('md'))
-
 	return (
 		<BackgroundWrapper>
 			<Grid container direction="column" justifyContent="flex-end" sx={{ minHeight: '100vh', padding: 2 }}>
@@ -22,24 +17,7 @@ const Login = () => {
 						</MainCard>
 					</Grid>
 					<Grid item lg={3} mb={3} xs={12} sx={{ paddingLeft: 2 }}>
-						<FormWrapper>
-							<Grid container spacing={2} justifyContent="center">
-								<Grid item xs={12}>
-									<Grid container direction={matchDownSM ? 'column-reverse' : 'row'} alignItems="center" justifyContent="center">
-										<Grid item>
-											<Stack alignItems="center" justifyContent="center" spacing={1}>
-												<Typography color={theme.palette.secondary.main} gutterBottom variant={matchDownSM ? 'h3' : 'h3'}>
-													Buy/Sell Order
-												</Typography>
-											</Stack>
-										</Grid>
-									</Grid>
-								</Grid>
-								<Grid item xs={12}>
-									<OrderForm />
-								</Grid>
-							</Grid>
-						</FormWrapper>
+						<OrderForm />
 						<MainCard sx={{ marginTop: 2 }}>
 							<OrderHistory />
 						</MainCard>
--- src/views/trading/order/OrderForm.jsx
+++ src/views/trading/order/OrderForm.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useTheme } from '@mui/material/styles'
-import { Box, Button, FormControl, FormHelperText, InputLabel, OutlinedInput, Stack, Typography } from '@mui/material'
+import {
+	Box,
+	Button,
+	FormControl,
+	FormHelperText,
+	Grid,
+	InputLabel,
+	OutlinedInput,
+	Stack,
+	ToggleButton,
+	ToggleButtonGroup,
+	Typography,
+} from '@mui/material'
 import * as Yup from 'yup'
 import { Formik } from 'formik'
 import useScriptRef from 'hooks/useScriptRef'
 import AnimateButton from 'ui-component/extended/AnimateButton'
 import { EXECUTE_TRANSACTION } from 'store/actions'
+import FormWrapper from './FormWrapper'
 
 const OrderTypes = {
 	Buy: 'Buy',
 	Sell: 'Sell',
+}
+
+const OrderCategories = {
+	Market: 'Market',
+	Limit: 'Limit',
 }
 
 const OrderForm = ({ ...others }) => {
@@ -19,6 +37,7 @@ const OrderForm = ({ ...others }) => {
 	const trading = useSelector((state) => state.trading)
 	const dispatch = useDispatch()
 	const [orderType, setOrderType] = useState(OrderTypes.Buy)
+	const [orderCategory, setOrderCategory] = useState(OrderCategories.Market)
 
 	const handleOrder = (values, setErrors) => {
 		if (orderType == OrderTypes.Buy && trading.cash < values.quantity * trading.price) {
@@ -37,111 +56,152 @@ const OrderForm = ({ ...others }) => {
 		})
 	}
 
+	const handleOrderTypeClick = (_, orderType) => {
+		if (orderType != null) {
+			setOrderType(orderType)
+		}
+	}
+
+	const handleOrderCategoryClick = (_, orderCategory) => {
+		if (orderCategory != null) {
+			setOrderCategory(orderCategory)
+		}
+	}
+
 	return (
-		<>
-			<Formik
-				initialValues={{
-					quantity: 10,
-					submit: null,
-				}}
-				validationSchema={Yup.object().shape({
-					quantity: Yup.number().required('Quantity is required').positive().integer(),
-				})}
-				onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
-					try {
-						if (scriptedRef.current) {
-							handleOrder(values, setErrors)
-							setStatus({ success: true })
-							setSubmitting(false)
-						}
-					} catch (err) {
-						console.error(err)
-						if (scriptedRef.current) {
-							setStatus({ success: false })
-							setErrors({ submit: err.message })
-							setSubmitting(false)
-						}
-					}
-				}}
-			>
-				{({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
-					<form noValidate onSubmit={handleSubmit} {...others}>
-						<FormControl fullWidth error={Boolean(touched.quantity && errors.quantity)} sx={{ ...theme.typography.customInput }}>
-							<InputLabel htmlFor="outlined-adornment-quantity">Quantity</InputLabel>
-							<OutlinedInput
-								id="outlined-adornment-quantity"
-								type="number"
-								value={values.quantity}
-								name="quantity"
-								onBlur={handleBlur}
-								onChange={handleChange}
-								label="Quantity"
-								inputProps={{ step: '1' }}
-								autoComplete="off"
-							/>
-							{touched.quantity && errors.quantity && (
-								<FormHelperText error id="standard-weight-helper-text-quantity">
-									{errors.quantity}
-								</FormHelperText>
-							)}
-						</FormControl>
-						<Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1}>
-							<Typography>
-								{'Price: '}
-								{trading.price.toLocaleString(undefined, {
-									maximumFractionDigits: 2,
-								})}
-							</Typography>
-							<Typography>
-								{'Amt: '}
-								{(values.quantity * trading.price).toLocaleString(undefined, {
-									maximumFractionDigits: 2,
-								})}
-							</Typography>
-						</Stack>
+		<FormWrapper>
+			<Grid container spacing={2} justifyContent="center">
+				<Grid item xs={12}>
+					<Formik
+						initialValues={{
+							quantity: 10,
+							submit: null,
+						}}
+						validationSchema={Yup.object().shape({
+							quantity: Yup.number().required('Quantity is required').positive().integer(),
+						})}
+						onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+							try {
+								if (scriptedRef.current) {
+									handleOrder(values, setErrors)
+									setStatus({ success: true })
+									setSubmitting(false)
+								}
+							} catch (err) {
+								console.error(err)
+								if (scriptedRef.current) {
+									setStatus({ success: false })
+									setErrors({ submit: err.message })
+									setSubmitting(false)
+								}
+							}
+						}}
+					>
+						{({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+							<form noValidate onSubmit={handleSubmit} {...others}>
+								<ToggleButtonGroup fullWidth value={orderType} exclusive onChange={handleOrderTypeClick}>
+									<ToggleButton value={OrderTypes.Buy} color="success">
+										{OrderTypes.Buy}
+									</ToggleButton>
+									<ToggleButton value={OrderTypes.Sell} color="error">
+										{OrderTypes.Sell}
+									</ToggleButton>
+								</ToggleButtonGroup>
+								<ToggleButtonGroup fullWidth value={orderCategory} exclusive onChange={handleOrderCategoryClick} sx={{ mt: 2 }}>
+									<ToggleButton value={OrderCategories.Market} color="success">
+										{OrderCategories.Market}
+									</ToggleButton>
+									<ToggleButton value={OrderCategories.Limit} color="error">
+										{OrderCategories.Limit}
+									</ToggleButton>
+								</ToggleButtonGroup>
 
-						{errors.submit && (
-							<Box sx={{ mt: 3 }}>
-								<FormHelperText error>{errors.submit}</FormHelperText>
-							</Box>
+								<FormControl
+									fullWidth
+									error={Boolean(touched.quantity && errors.quantity)}
+									sx={{ ...theme.typography.customInput, mt: 2 }}
+								>
+									<InputLabel htmlFor="outlined-adornment-quantity">Quantity</InputLabel>
+									<OutlinedInput
+										id="outlined-adornment-quantity"
+										type="number"
+										value={values.quantity}
+										name="quantity"
+										onBlur={handleBlur}
+										onChange={handleChange}
+										label="Quantity"
+										inputProps={{ step: '1' }}
+										autoComplete="off"
+									/>
+									{touched.quantity && errors.quantity && (
+										<FormHelperText error id="standard-weight-helper-text-quantity">
+											{errors.quantity}
+										</FormHelperText>
+									)}
+								</FormControl>
+								<Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1}>
+									<Typography>
+										{'Price: '}
+										{trading.price.toLocaleString(undefined, {
+											maximumFractionDigits: 2,
+										})}
+									</Typography>
+									<Typography>
+										{'Amt: '}
+										{(values.quantity * trading.price).toLocaleString(undefined, {
+											maximumFractionDigits: 2,
+										})}
+									</Typography>
+								</Stack>
+
+								{errors.submit && (
+									<Box sx={{ mt: 3 }}>
+										<FormHelperText error>{errors.submit}</FormHelperText>
+									</Box>
+								)}
+
+								{orderType == OrderTypes.Buy && (
+									<Box sx={{ mt: 2 }}>
+										<AnimateButton>
+											<Button
+												disableElevation
+												disabled={isSubmitting}
+												fullWidth
+												size="large"
+												type="submit"
+												variant="contained"
+												color="success"
+												onClick={() => setOrderType(OrderTypes.Buy)}
+											>
+												Buy
+											</Button>
+										</AnimateButton>
+									</Box>
+								)}
+								{orderType == OrderTypes.Sell && (
+									<Box sx={{ mt: 2 }}>
+										<AnimateButton>
+											<Button
+												disableElevation
+												disabled={isSubmitting}
+												fullWidth
+												size="large"
+												type="submit"
+												variant="contained"
+												color="error"
+												onClick={() => setOrderType(OrderTypes.Sell)}
+											>
+												Sell
+											</Button>
+										</AnimateButton>
+									</Box>
+								)}
+							</form>
 						)}
-
-						<Box sx={{ mt: 2 }}>
-							<AnimateButton>
-								<Button
-									disableElevation
-									disabled={isSubmitting}
-									fullWidth
-									size="large"
-									type="submit"
-									variant="contained"
-									color="success"
-									onClick={() => setOrderType(OrderTypes.Buy)}
-								>
-									Buy
-								</Button>
-							</AnimateButton>
-						</Box>
-						<Box sx={{ mt: 1 }}>
-							<AnimateButton>
-								<Button
-									disableElevation
-									disabled={isSubmitting}
-									fullWidth
-									size="large"
-									type="submit"
-									variant="contained"
-									color="error"
-									onClick={() => setOrderType(OrderTypes.Sell)}
-								>
-									Sell
-								</Button>
-							</AnimateButton>
-						</Box>
-					</form>
-				)}
-			</Formik>
-		</>
+					</Formik>
+				</Grid>
+			</Grid>
+		</FormWrapper>
 	)
 }
 
